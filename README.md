305997b14c4ec03053c0c6d9b5ae487edf900e73
LynaSovann <sovannlyna2004@gmail.com>
2025-02-20 15:21:24 +0700

    add image as well

--- README.md
+++ README.md
@@ -38,6 +38,28 @@ const getOneUser = async (userId) => {
   console.log(await jsonData);
 };
 ```
-### 
+### Add new user
+```js
+const addNewUser = (user) => {
+  fetch(BASE_URL, {
+    method: "POST",
+    body: JSON.stringify(user)
+  })
+    .then((data) => data.json())
+    .then((data) => console.log(data));
+};
+
+addNewUser({
+  name: "Lyna",
+  username: "lyna_sovann",
+  email: "sovannlyna2004@gmail.com",
+  address: {},
+  phone: "0123456789",
+  website: "myblog.fake.website",
+  company: {},
+});
+```
+Result 
+![Alt text](imgs/add-result.png)
 
 ## XML HttpRequest
--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const BASE_URL = "https://jsonplaceholder.typicode.com/users";
 
-const getOneUser = async (userId) => {
-  const user = await fetch(`${BASE_URL}/${userId}`);
-  const jsonData = await user.json();
-  console.log(await jsonData);
+const addNewUser = (user) => {
+  fetch(BASE_URL, {
+    method: "POST",
+    body: JSON.stringify(user)
+  })
+    .then((data) => data.json())
+    .then((data) => console.log(data));
 };
 
-getOneUser(2);
+addNewUser({
+  name: "Lyna",
+  username: "lyna_sovann",
+  email: "sovannlyna2004@gmail.com",
+  address: {},
+  phone: "0123456789",
+  website: "myblog.fake.website",
+  company: {},
+});
